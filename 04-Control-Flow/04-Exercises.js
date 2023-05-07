79af061ca677c346e1d4808c42588f75a66e4e0d
Adnan Khan <adnan.khan2@kyndryl.com>
2023-05-07 20:43:22 +0500

    Fix fizzbuzz

--- 04-Control-Flow/04-Exercises.js
+++ 04-Control-Flow/04-Exercises.js
@@ -32,9 +32,9 @@ function fizzbuzz(number) {
 
 function fizzBuzz(input) {
   if (typeof input !== "number") return "Not a number";
+  if (input % 3 === 0 && input % 5 === 0) return "FizzBuzz";
   if (input % 3 === 0) return "Fizz";
   if (input % 5 === 0) return "Buzz";
-  if (input % 3 === 0 && input % 5 === 0) return "FizzBuzz";
   return input;
 }
 
